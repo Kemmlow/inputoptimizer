0ea2ddb79b25e272c6328d2337c1573c7e400341
Kemmlow <sliceSplit@outlook.com>
2026-08-03 21:23:29 +0530

    crash fix

--- src/main/java/dev/kemmlow/inputoptimizer/mixin/MinecraftAccessor.java
+++ src/main/java/dev/kemmlow/inputoptimizer/mixin/MinecraftAccessor.java
@@ -1,12 +1,17 @@
 package dev.kemmlow.inputoptimizer.mixin;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(Minecraft.class)
 public interface MinecraftAccessor {
-    @Accessor("screen")
-    Screen getScreen();
+    @Accessor("gui")
+    Gui getGui();
+
+    default Screen getScreen() {
+        return getGui().screen();
+    }
 }
